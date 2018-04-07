cb3cf7567318a54b167ab2aba58bf5629e4c6848
Bill Dybas <wmdybas@gmail.com>
2018-04-07 13:38:10 -0400

    Route Ideas

--- src/http/responses/error.js
+++ src/http/responses/error.js
@@ -6,6 +6,10 @@ const BaseError = (res, status, message) => {
     })
 }
 
+export const BadRequestError = (res) => {
+  BaseError(res, 400, 'Bad Request')
+}
+
 // 404
 export const NotFoundError = (res) => {
   BaseError(res, 404, 'Not Found')
--- src/http/routes/index.js
+++ src/http/routes/index.js
@@ -1,9 +1,38 @@
 import express from 'express'
 
+import {
+  BadRequestError
+} from '../responses/error'
+
 const router = express.Router()
 
-router.use('/', (req, res) => {
-  res.json({message: 'hello world'})
-})
+router
+  .route('/fetch')
+  .post((req, res, next) => {
+    if (!req.body.url) {
+      BadRequestError(res)
+    }
+
+    // TODO:
+    // - parse the URL, fetch its data, and return that data
+  })
+
+router
+  .route('/parse')
+  .post((req, res, next) => {
+    if (!req.body.url) {
+      BadRequestError(res)
+    }
+
+    // TODO:
+    // - parse the URL and return the parsed object form
+  })
+
+router
+  .route('/:provider/:id')
+  .get((req, res, next) => {
+    // TODO:
+    // - fetch the data at the given provider and id
+  })
 
 export default router
