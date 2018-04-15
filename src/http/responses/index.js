4c466e07d2696fad446911d24a2961cd9487f671
Bill Dybas <wmdybas@gmail.com>
2018-04-15 02:51:29 -0400

    Add 'meta' to Responses

--- src/http/responses/index.js
+++ src/http/responses/index.js
@@ -4,11 +4,11 @@ const Response = (res, status, payload) => {
     .json({...payload})
 }
 
-const StructuredResponse = (res, status, data) => {
-  Response(res, status, { status, data })
+const StructuredResponse = (res, status, data, meta = undefined) => {
+  Response(res, status, { status, data, meta })
 }
 
 // 200
-export const SuccessResponse = (res, data) => {
-  StructuredResponse(res, 200, data)
+export const SuccessResponse = (res, data, meta = undefined) => {
+  StructuredResponse(res, 200, data, meta)
 }
