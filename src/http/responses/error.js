aa041983bf4a16de136a44eb97eec223c3ae086c
Bill Dybas <wmdybas@gmail.com>
2018-04-15 02:51:04 -0400

    Add More Errors

--- src/http/responses/error.js
+++ src/http/responses/error.js
@@ -6,6 +6,7 @@ const BaseError = (res, status, message) => {
     })
 }
 
+// 400
 export const BadRequestError = (res) => {
   BaseError(res, 400, 'Bad Request')
 }
@@ -13,4 +14,14 @@ export const BadRequestError = (res) => {
 // 404
 export const NotFoundError = (res) => {
   BaseError(res, 404, 'Not Found')
+}
+
+// 422
+export const UnprocessableEntityError = (res) => {
+  BaseError(res, 422, 'Unprocessable Entity')
+}
+
+// 422
+export const UnknownProviderError = (res) => {
+  BaseError(res, 422, 'Unknown Provider')
 }
