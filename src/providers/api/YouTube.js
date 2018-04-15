cf44df638136a6b60b30388543b408a85b721089
Bill Dybas <wmdybas@gmail.com>
2018-04-15 01:52:17 -0400

    Add Error Handling to YouTube Provider

--- src/providers/api/YouTube.js
+++ src/providers/api/YouTube.js
@@ -18,17 +18,27 @@ class YouTubeAPIProvider extends Provider {
       ...(opts.maxWidth ? { 'maxWidth': opts.maxWidth } : {}) // Optional
     }
 
-    // Make the call
-    const response = await this.client.get(url, {params})
-    // YouTube returns an 'items' array
-    const data = response.data.items
-    // If the array is empty, then no video was found with the provided 'id'
-    if (data.length === 0) {
+    // Endpoint Returns:
+    // - 200: Video found and returned
+    // - 400: Video chart not found (N/A)
+    // - 403: Forbidden (N/A)
+    // - 404: Video not found (N/A - If the video isn't found, returns a 200 w/ an empty array)
+    try {
+      // Make the call
+      const response = await this.client.get(url, {params})
+      // YouTube returns an 'items' array
+      const data = response.data.items
+      // If the array is empty, then no video was found with the provided 'id'
+      if (data.length === 0) {
+        return null
+      }
+
+      // Otherwise, the array should have 1 element – the video data
+      return data[0]
+    } catch (e) {
+      console.error(e)
       return null
     }
-
-    // Otherwise, the array should have 1 element – the video data
-    return data[0]
   }
 }
 
